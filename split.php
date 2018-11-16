d70c8635b2a8114b8c8103fd6957ca7a077d4c24
GiiAAA <39315356+GiiAAA@users.noreply.github.com>
2018-11-16 17:40:51 -0200

    Add files via upload

---
+++ split.php
@@ -0,0 +1,51 @@
+<!--
+# array str_split (string $string[, int $split_length])			
+		 -->
+
+<?php
+	
+	#$tamanho = "caracteres";
+	#echo count($tamanho),"<br>";
+	
+	#$texto = "Ola,Mundo! <br>" ;
+
+	#$arr = str_split($texto,1);
+	
+	#echo "<pre>";
+	#print_r ($arr);
+	#echo "</pre><br>";
+	
+	#echo "O número de caracteres da string eh: ", count($arr),"<br>";
+	
+		
+	#$palavra = "computador";
+	
+	#echo "<pre>";
+	#print_r (str_split($palavra));
+	#echo "</pre><br>";
+	
+	
+	#$letra = "a";	
+	#echo "O valor de [$letra] na Tabela ASCII É: " ,ord($letra);
+	#echo "<br>";	
+	
+	#echo "<pre>";
+	#foreach (str_split($palavra) as $value){
+		
+	#echo "O valor de [$value] na Tabela ASCII É: " ,ord($value),"<br>"; }
+		
+		
+	
+	#echo "<pre>";
+	
+	#echo ("==========================<br>");
+	
+	#$cor = array('Preto', 'Branco', 'Vermelho', 'Verde', 'Azul');
+	#echo "<pre>";
+	#print_r($cor);
+	#echo "</pre><br>";
+	
+	#echo "Minha cor Preferida é {$cor[2]}!<br>";			
+	
+	
+?>
